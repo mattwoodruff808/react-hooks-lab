6428267b82b91e94f83656e32f2b620687a92e8d
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-21 21:42:56 -0700

    Finished app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import ListItem from './Components/ListItem';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => {
+      setPokemon(res.data.results)
+    })
+  }, [])
+
+  const mappedPokemon = pokemon.map((el, i) => {
+    return <ListItem key={i} pokemon={el.name} />
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {mappedPokemon}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ app-2/src/Components/ListItem.js
@@ -0,0 +1,9 @@
+const ListItem = (props) => {
+    return (
+        <div>
+            <h2>{props.pokemon}</h2>
+        </div>
+    )
+}
+
+export default ListItem;
